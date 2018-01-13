afa5dfc5368b3c856d6d1373feab40ad2ce8652a
Jarosław Komar <jaszczur666@o2.pl>
2018-01-13 13:11:45 +0100

    Added previously developed code

---
+++ Multiplets.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+public class Multiplets
+{
+    List<StarkBuilder.Multiplet> MultipletList;
+    //List<List<int>> levels = new List<List<int>>();
+
+    public Multiplets()
+	{
+        
+	}
+    override public string  ToString()
+    {
+        string rep="";
+        foreach (StarkBuilder.Multiplet level in MultipletList)
+        {
+            rep += level.Label+" ";
+            foreach (int stark in level.Components)
+            {
+                rep += stark.ToString() + " ";
+            }
+            rep += "\r\n";
+        }
+        return rep;
+
+    }
+    public void LoadFromString(string data) {
+        MultipletList = new List<StarkBuilder.Multiplet>();
+        foreach (var myString in data.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+        {
+            //   Console.WriteLine(myString);
+            StarkBuilder.Multiplet Multi=new StarkBuilder.Multiplet();
+            Multi.Label=myString.Split(' ')[0];
+            string lista=myString.Split(' ')[1];
+            List<int> level = new List<int>();
+            foreach (string value in lista.Split(','))
+            {
+                level.Add(Int32.Parse(value));
+                
+            }
+            Multi.Components=level;
+            MultipletList.Add(Multi);
+        }
+    }
+    public string GetSafeFilename(string filename)
+    {
+
+        return string.Join("_", filename.Split(System.IO.Path.GetInvalidFileNameChars()));
+
+    }
+    public void DumpFiles()
+    {
+        int size = MultipletList.Count;
+        for (int krok=size-1; krok >= 0; krok--)
+        {
+            for (int j = krok - 1; j >= 0; j--)
+            {
+                string name=MultipletList[krok].Label + "_" + MultipletList[j].Label+".txt";
+                string content = "";
+                foreach (int stark in MultipletList[krok].Components) content +=stark.ToString() +" ";
+                content += "\r\n";
+                foreach (int stark in MultipletList[j].Components) content += stark.ToString() + " ";
+                string fname = GetSafeFilename(name);
+                Console.WriteLine(fname);
+                Console.WriteLine(content);
+                System.IO.File.WriteAllText(fname, content);
+            }
+            //Console.WriteLine(MultipletList[krok].Label);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace StarkBuilder
+{
+    class Program
+    {
+        static Multiplets MultiData=new Multiplets();
+        static void Main(string[] args)
+        {
+//            foreach (string arg in args)
+//                Console.WriteLine(arg);
+            if (args.Length==1)
+            {
+                string data = System.IO.File.ReadAllText(args[0]);
+                //Console.WriteLine(data);
+                //Console.WriteLine("___________________________________________________________");
+                MultiData.LoadFromString(data);
+                //Console.WriteLine(MultiData.ToString());
+                MultiData.DumpFiles();
+            }
+        }
+    }
+}
